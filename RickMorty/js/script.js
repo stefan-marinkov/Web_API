ba5db94eb70887e84884eeea32890b1df463223a
stefan-marinkov <72408345+stefan-marinkov@users.noreply.github.com>
2021-03-20 08:50:50 +0100

    pagination
    pagination

--- RickMorty/js/script.js
+++ RickMorty/js/script.js
@@ -5,87 +5,136 @@ const otherPage = selectEl('.oneCharacter')
 const prevBtn = selectEl('.prev')
 const nextBtn = selectEl('.next')
 const input = selectEl('input')
-const btnInput = selectEl('.btnS')
+const pagination = selectEl('.pagination')
+
+let totalPages = 34;
+
+
+
+
+const onClickButton = button => {
+    let currentPage = button.textContent
+
+    pagination.innerHTML = ''
+
+    createButtons(currentPage)
+    let baseUrl = "https://rickandmortyapi.com/api/character/?page=" + currentPage
+    getRandM(baseUrl)
+    .then( data => {
+        allData = data; 
+        getAllChar(data)})
+
+        
+
+    
+}
+
+const makeButton = (number, currentPage) => {
+    let button = document.createElement('li')
+    button.textContent = number
+    button.classList.add('pug')
+
+    button.addEventListener('click', () => onClickButton(button))
+    number == currentPage && button.classList.add('active')
+    pagination.appendChild(button)
+}
+
+const createButtons = currentPage => {
+    
+    for( let i = 0; i < 5; i++) {
+        let number;
+
+        if( !currentPage || currentPage <= 3) {
+            number = i + 1
+        } else if ( currentPage >= totalPages - 2) {
+            number = totalPages - 4 + i
+        } else {
+            number = currentPage -2 + i
+        }
+        makeButton(number, currentPage)
+}}
+createButtons()
+
+let allData;
+
+input.addEventListener('keyup', () => {
+    const newData = allData.filter(res => {
+        
+        return res.name.toLowerCase().includes(input.value.toLowerCase())
+    })
+    getAllChar(newData)
+})
+
 
 let count = 1
 let baseUrl = "https://rickandmortyapi.com/api/character/?page=" + count
 
-//NEXT PAGE
-nextBtn.addEventListener( 'click', () => {
-    count < 34 ? count ++ : count
-    let baseUrl = "https://rickandmortyapi.com/api/character/?page=" + count
-    getRandM(baseUrl)
-    .then( data => getAllChar(data))
-})
-//PREV PAGE
-prevBtn.addEventListener( 'click', () => {
-    count > 0 ? count -- : count
-    let baseUrl = "https://rickandmortyapi.com/api/character/?page=" + count
-    getRandM(baseUrl)
-    .then( data => getAllChar(data))
-})
+
 // BASE URL FETCH FOR ALL CHAR
 getRandM(baseUrl)
-.then( data => getAllChar(data))
+.then( data => {
+    allData = data; 
+    getAllChar(data)
+})
 
 
 async function getRandM(url) {
 
     const resolve = await fetch( url)
     const data = await resolve.json()
-    return data
+    return data.results
 }
 // ALL CHARACTER FUNCTION
 
 function getAllChar(data) {
     wrap.innerHTML = ''
-    data.results.forEach( char => {
+    data.forEach( char => {
         const divChar = createEl('div', 'char')
         divChar.innerHTML = `<p>${char.name}</p>
         <img class='charImg' src='${char.image}'>
         <button class='btn'>Info</button>`
         wrap.appendChild(divChar)
         // GET SINGLE CHARACTER
-divChar.addEventListener( 'click', () => {
+// divChar.addEventListener( 'click', () => {
 
-    let singUrl = ("https://rickandmortyapi.com/api/character/" + char.id);
+//     let singUrl = ("https://rickandmortyapi.com/api/character/" + char.id);
 
-    getSingChar(singUrl)
-    .then(data => singleChar(data))
+//     getSingChar(singUrl)
+//     .then(data => singleChar(data))
     
 
-    async function getSingChar(url) {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
-    }
-    // GET SINGLE CHARACTER FUNCTION
+//     async function getSingChar(url) {
+//         const response = await fetch(url)
+//         const data = await response.json()
+//         return data
+//     }
+//     // GET SINGLE CHARACTER FUNCTION
     
-    function singleChar(data) {
-        otherPage.innerHTML = ''
-        const characterInfo = createEl('div', 'singCh')
-        characterInfo.innerHTML = `
-            <img class='sinImg' src='${data.image}'>
-            <div class='charInfo'>
-                <p>Name: ${data.name}</p>
-                <p>Status: ${data.status}</p>
-                <p>Species: ${data.species}</p>
-                <p>Origin: ${data.origin.name}</p>
-                <p>Location: ${data.location.name}</p>
-            </div>
-            <button class='back' onclick="">Back To All Character</button>   
-        `
-        otherPage.classList.toggle('active')
-        otherPage.appendChild(characterInfo)
-        wrap.style.display = 'none'
-        // BACK TO ALL CHARACTER
-        const btn = selectEl('.back')
-        btn.addEventListener('click', () => {
-        wrap.style.display = 'flex'
-        characterInfo.style.display = 'none'
-        otherPage.classList.toggle('active')
-                })
-            }
-        })
+//     function singleChar(data) {
+//         otherPage.innerHTML = ''
+//         const characterInfo = createEl('div', 'singCh')
+//         characterInfo.innerHTML = `
+//             <img class='sinImg' src='${data.image}'>
+//             <div class='charInfo'>
+//                 <p>Name: ${data.name}</p>
+//                 <p>Status: ${data.status}</p>
+//                 <p>Species: ${data.species}</p>
+//                 <p>Origin: ${data.origin.name}</p>
+//                 <p>Location: ${data.location.name}</p>
+//             </div>
+//             <button class='back' onclick="">Back To All Character</button>   
+//         `
+//         otherPage.classList.toggle('active')
+//         otherPage.appendChild(characterInfo)
+//         wrap.style.display = 'none'
+//         // BACK TO ALL CHARACTER
+//         const btn = selectEl('.back')
+//         btn.addEventListener('click', () => {
+//         wrap.style.display = 'flex'
+//         characterInfo.style.display = 'none'
+//         otherPage.classList.toggle('active')
+//                 })
+//             }
+//         })
     })
 }
